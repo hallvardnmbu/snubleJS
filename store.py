467e8e048276f4da31d759331885a57d60a5e66d
Hallvard Lavik <hallvard.hoyland.lavik@nmbu.no>
2024-07-12 14:50:50 +0200

    Rename variables and updated docstrings.

--- store.py
+++ store.py
@@ -6,57 +6,71 @@ from scrape import CATEGORY, get_products
 
 def store_products(
         category: CATEGORY = CATEGORY.RED_WINE,
-        products_filename: str = "products.csv",
-        prices_filename: str = "prices.csv"
+        products: str = "products.csv",
+        prices: str = "prices.csv"
 ):
     """
-    Fetches all products from the given category and stores them in a CSV file.
+    Fetches all products from the given category and stores them in file `products` (overwrites).
+    Updates the prices in file `prices` with the newly fetched values (appends).
 
     Parameters
     ----------
-    category : CATEGORY
+    category : CATEGORY, optional
         The category of products to fetch.
-    products_filename : str
-        The name of the CSV file to store the products.
-    prices_filename : str
-        The name of the CSV file to store the prices updated prices.
+    products : str, optional
+        The name of the CSV file to store the products. Overwrites the file if it exists.
+    prices : str, optional
+        The name of the CSV file to store the updated prices.
     """
-    products = get_products(category)
-    df = pd.DataFrame(products).T
-    df.to_csv(products_filename)
+    _products = get_products(category)
+    _products = pd.DataFrame(_products).T
+    _products.to_csv(products)
 
-    _store_prices(df, prices_filename)
+    _store_prices(_products, prices)
 
 
-def _store_prices(products: pd.DataFrame, filename: str):
+def _store_prices(
+        products: pd.DataFrame,
+        file: str
+):
+    """
+    Updates (appends) the prices in the given file with the new values.
+
+    Parameters
+    ----------
+    products : pd.DataFrame
+        The products with their current metadata.
+    file : str
+        The name of the CSV file to store the updated prices.
+    """
     products.reset_index(inplace=True, names="id")
     products.set_index(['name', 'volume', 'country', 'district', 'sub_district'], inplace=True)
     products.rename(columns={"price": pd.Timestamp.now()}, inplace=True)
 
-    if not os.path.exists(filename):
-        products.to_csv(filename)
+    if not os.path.exists(file):
+        products.to_csv(file)
         return
 
-    old = pd.read_csv(filename, index_col=['name', 'volume', 'country', 'district', 'sub_district'])
+    old = pd.read_csv(file, index_col=['name', 'volume', 'country', 'district', 'sub_district'])
 
     updated = pd.concat([old, products], axis=1)
-    updated.to_csv(filename)
+    updated.to_csv(file)
 
 
 def store_prices(
         category: CATEGORY = CATEGORY.RED_WINE,
-        filename: str = "prices.csv"
+        file: str = "prices.csv"
 ):
     """
-    Fetches all products from the given category and updates the prices in the given CSV file.
+    Fetches all products from the given category and updates (appends) their prices in `file`.
 
     Parameters
     ----------
-    category : CATEGORY
+    category : CATEGORY, optional
         The category of products to fetch.
-    filename : str
-        The name of the CSV file to store the prices updated prices.
+    file : str, optional
+        The name of the CSV file to store the updated prices.
     """
     products = get_products(category)
     products = pd.DataFrame(products).T
-    _store_prices(products, filename)
+    _store_prices(products, file)
