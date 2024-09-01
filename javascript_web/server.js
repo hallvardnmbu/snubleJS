9843a4f7a0ce583c6a40775d267f4264743a449c
Hallvard Lavik <hallvard.hoyland.lavik@nmbu.no>
2024-09-01 20:43:07 +0200

    Improve load functionality.

--- javascript_web/server.js
+++ javascript_web/server.js
@@ -1,7 +1,7 @@
-import express from 'express';
-import { MongoClient } from 'mongodb';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import express from "express";
+import { MongoClient } from "mongodb";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -11,41 +11,162 @@ const port = 3000;
 
 let collection;
 
-const uri = `mongodb+srv://web:ByiT9WakPCj8izEO@vinskraper.wykjrgz.mongodb.net/`; // Replace with your MongoDB URI
+const uri = `mongodb+srv://web:ByiT9WakPCj8izEO@vinskraper.wykjrgz.mongodb.net/`;
 
+async function load({
+  collection,
+  kategori = [],
+  underkategori = [],
+  land = [],
+  distrikt = [],
+  underdistrikt = [],
+  volum = [],
+  argang = [],
+  beskrivelse = [],
+  kork = [],
+  lagring = [],
+  butikk = [],
+  passer_til = [],
+  alkoholfritt = true,
+  alkohol = null,
+  fra = null,
+  til = null,
+  sorting = "prisendring 2024-08-01",
+  ascending = true,
+  amount = 10,
+  page = 1,
+  search = null,
+  filters = false,
+  fresh = true,
+} = {}) {
+  let pipeline = [];
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-app.use(express.static(path.join(__dirname, 'public')));
+  if (search) {
+    pipeline.push({
+      $search: {
+        index: "navn",
+        text: {
+          query: search,
+          path: "navn",
+        },
+      },
+    });
+  }
+
+  let matchStage = {
+    utgått: false,
+    "kan kjøpes": true,
+    ...(butikk.length && filters ? { butikk: { $all: butikk } } : {}),
+    ...(passer_til.length && filters ? { "passer til": { $all: passer_til } } : {}),
+    ...(kategori.length && filters ? { kategori: { $in: kategori } } : {}),
+    ...(underkategori.length && filters ? { underkategori: { $in: underkategori } } : {}),
+    ...(volum.length && filters ? { volum: { $in: volum.map((v) => parseFloat(v)) } } : {}),
+    ...(land.length && filters ? { land: { $in: land } } : {}),
+    ...(distrikt.length && filters ? { distrikt: { $in: distrikt } } : {}),
+    ...(underdistrikt.length && filters ? { underdistrikt: { $in: underdistrikt } } : {}),
+    ...(argang.length && filters
+      ? { årgang: { $in: argang.map((ar) => parseInt(ar.replace(".0", ""))) } }
+      : {}),
+    ...(beskrivelse.length && filters ? { "beskrivelse.kort": { $in: beskrivelse } } : {}),
+    ...(kork.length && filters ? { kork: { $in: kork } } : {}),
+    ...(lagring.length && filters ? { lagring: { $in: lagring } } : {}),
+  };
+
+  if (alkohol !== null) {
+    matchStage["alkohol"] = { ...matchStage["alkohol"], $gte: alkohol };
+  }
+  if (!alkoholfritt) {
+    matchStage["alkohol"] = { ...matchStage["alkohol"], $ne: null };
+  }
+
+  if (fra !== null || til !== null) {
+    let between = {};
+    if (fra !== null) between["$gte"] = fra;
+    if (til !== null) between["$lte"] = til;
+
+    if (sorting === "volum") {
+      matchStage["volum"] = { ...matchStage["volum"], ...between };
+    } else {
+      matchStage[sorting] = between;
+    }
+  }
+
+  matchStage[sorting] = { ...matchStage[sorting], $exists: true, $ne: null };
+
+  pipeline.push({ $match: matchStage });
+
+  let total;
+  if (fresh) {
+    const tot = await collection.aggregate([...pipeline, { $count: "amount" }]).toArray();
+    if (tot.length === 0) {
+      throw new Error("No records found.");
+    }
+    total = Math.floor(tot[0].amount / amount);
+  } else {
+    total = null;
+  }
+
+  pipeline.push(
+    { $sort: { [sorting]: ascending ? 1 : -1 } },
+    { $skip: (page - 1) * amount },
+    { $limit: amount },
+  );
+
+  const data = await collection.aggregate(pipeline).toArray();
+
+  return { data, total };
+}
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
 
 MongoClient.connect(uri)
-  .then(client => {
-    const db = client.db('vinskraper'); // Replace with your database name
-    collection = db.collection('varer'); // Replace with your collection name
+  .then((client) => {
+    const db = client.db("vinskraper"); // Replace with your database name
+    collection = db.collection("varer"); // Replace with your collection name
 
     // Route to display products with pagination
-    app.get('/', async (req, res) => {
+    app.get("/", async (req, res) => {
       try {
         const page = parseInt(req.query.page) || 1; // Get the current page number from query string, default to 1
         const limit = 10; // Number of products to display per page
-        const skip = (page - 1) * limit; // Calculate the number of products to skip
 
-        const documents = await collection.find({})
-          .skip(skip)
-          .limit(limit)
-          .toArray();
+        let { data, total } = await load({
+          collection: collection,
+          kategori: [],
+          underkategori: [],
+          land: [],
+          distrikt: [],
+          underdistrikt: [],
+          volum: [],
+          argang: [],
+          beskrivelse: [],
+          kork: [],
+          lagring: [],
+          butikk: [],
+          passer_til: [],
+          alkoholfritt: true,
+          alkohol: null,
+          fra: null,
+          til: null,
+          sorting: "prisendring 2024-08-01",
+          ascending: true,
+          amount: limit,
+          page: page,
+          search: "",
+          filters: false,
+          fresh: true,
+        });
 
-        const totalDocuments = await collection.countDocuments(); // Get the total number of documents
-        const totalPages = Math.ceil(totalDocuments / limit);
-
-        res.render('index', { 
-          data: documents,
+        res.render("index", {
+          data: data,
           currentPage: page,
-          totalPages: totalPages
+          totalPages: total,
         });
       } catch (err) {
         console.error(err);
-        res.status(500).send('Error fetching data from MongoDB');
+        res.status(500).send("Error fetching data from MongoDB");
       }
     });
 
@@ -53,4 +174,4 @@ MongoClient.connect(uri)
       console.log(`Server running at http://localhost:${port}`);
     });
   })
-  .catch(error => console.error(error));
+  .catch((error) => console.error(error));
