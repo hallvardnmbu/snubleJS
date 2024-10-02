2758e640952f7de70be9354898625ee4e120a43e
Chebbbing <hebb@itu.dk>
2024-10-02 19:56:06 +0200

    Info button

--- simple/public/js/script.js
+++ simple/public/js/script.js
@@ -29,3 +29,31 @@ function setVisibilityFromLocalStorage(ids) {
 document.addEventListener("DOMContentLoaded", function () {
   setVisibilityFromLocalStorage(["volumeForm", "newsForm", "disclaimer-1", "disclaimer-2"]);
 });
+
+
+// Get modal element
+var modal = document.getElementById("infoModal");
+
+// Get button that opens the modal
+var btn = document.getElementById("info");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
