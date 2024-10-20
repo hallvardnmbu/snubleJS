ee3738bc78b0d67c36164a4432d484b96c57f591
Hallvard Lavik <hallvard.hoyland.lavik@nmbu.no>
2024-10-20 10:19:20 +0200

    Updated database modification code. Add function to quickly backup data.

--- database/refactor.py
+++ database/refactor.py
@@ -6,72 +6,103 @@ import pymongo
 from pymongo.mongo_client import MongoClient
 from pymongo.results import BulkWriteResult
 
+import pandas as pd
+import pyarrow
 
-_OLD = MongoClient(
-    f'mongodb+srv://{os.environ.get("MONGO_USR")}:{os.environ.get("MONGO_PWD")}'
-    f'@vinskraper.wykjrgz.mongodb.net/'
-    f'?retryWrites=true&w=majority&appName=vinskraper'
-)['vinskraper']
-_NEW = MongoClient(
+
+# _OLD = MongoClient(
+#     f'mongodb+srv://{os.environ.get("MONGO_USR")}:{os.environ.get("MONGO_PWD")}'
+#     f'@vinskraper.wykjrgz.mongodb.net/'
+#     f'?retryWrites=true&w=majority&appName=vinskraper'
+# )['vinskraper']
+# _DATABASE = _OLD['vinskraper']['varer']
+# _EXPIRED = _OLD['vinskraper']['utgått']
+_DATABASE = MongoClient(
     f'mongodb+srv://{os.environ.get("MONGO_USR")}:{os.environ.get("MONGO_PWD")}'
     f'@snublejuice.faktu.mongodb.net/'
     f'?retryWrites=true&w=majority&appName=snublejuice'
 )['snublejuice']['products']
-# _DATABASE = _CLIENT['vinskraper']['varer']
-# _EXPIRED = _CLIENT['vinskraper']['utgått']
+
 
 def update_prices(records) -> BulkWriteResult:
+    raise ValueError("ARE YOU SURE???")
+
+    backup()
+
     operations = [
         pymongo.UpdateOne(
             {'index': record['index']},
             [
-                {'$rename': {'pris': 'førpris'}},
-                {'$set': record},
-                {'$set': {'priser': {'$ifNull': ['$priser', []]}}},
-                {'$set': {'priser': {'$concatArrays': ['$priser', ['$pris']]}}},
-                {'$set': {
-                    'prisendring': {'$cond': [
-                        {'if': {'and': [
-                            {'$gt': ['$førpris', 0]},
-                            {'$gt': ['$pris', 0]}
-                        ]}},
-                        {'$multiply': [
-                            {'$divide': [
-                                {'$subtract': ['$pris', '$førpris']},
-                                '$førpris'
-                            ]},
-                            100
-                        ]},
-                        0
-                    ]},
-                }},
-                {'$set': {
-                    'literpris': {'$cond': {
-                        'if': {'$and': [
-                            {'$ne': ['$pris', None]},
-                            {'$ne': ['$pris', 0]},
-                            {'$ne': ['$volum', None]},
-                            {'$ne': ['$volum', 0]},
-                        ]},
-                        'then': {'$multiply': [
-                            {'$divide': ['$pris', '$volum']},
-                            100
-                        ]},
-                        'else': None
-                    }}
-                }},
-                {'$set': {
-                    'alkoholpris': {'$cond': {
-                        'if': {'$and': [
-                            {'$ne': ['$literpris', None]},
-                            {'$ne': ['$literpris', 0]},
-                            {'$ne': ['$alkohol', None]},
-                            {'$ne': ['$alkohol', 0]},
-                        ]},
-                        'then': {'$divide': ['$literpris', '$alkohol']},
-                        'else': None
-                    }}
-                }}
+              { "$set": { "oldprice": "$price" } },
+              { "$set": record },
+              { "$set": { "prices": { "$ifNull": ["$prices", []] } } },
+              { "$set": { "prices": { "$concatArrays": ["$prices", ["$price"]] } } },
+              {
+                  "$set": {
+                  "discount": {
+                    "$cond": {
+                      "if": {
+                        "$and": [
+                          { "$gt": ["$oldprice", 0] },
+                          { "$gt": ["$price", 0] },
+                          { "$ne": ["$oldprice", None] },
+                          { "$ne": ["$price", None] },
+                        ],
+                      },
+                      "then": {
+                        "$multiply": [
+                          {
+                            "$divide": [{ "$subtract": ["$price", "$oldprice"] }, "$oldprice"],
+                          },
+                          100,
+                        ],
+                      },
+                      "else": 0,
+                    },
+                  },
+                  "literprice": {
+                    "$cond": {
+                      "if": {
+                        "$and": [
+                          { "$gt": ["$price", 0] },
+                          { "$gt": ["$volume", 0] },
+                          { "$ne": ["$price", None] },
+                          { "$ne": ["$volume", None] },
+                        ],
+                      },
+                      "then": {
+                        "$multiply": [
+                          {
+                            "$divide": ["$price", "$volume"],
+                          },
+                          100,
+                        ],
+                      },
+                      "else": None,
+                    },
+                  },
+                },
+              },
+              {
+                  "$set": {
+                  "alcoholprice": {
+                    "$cond": {
+                      "if": {
+                        "$and": [
+                          { "$gt": ["$literprice", 0] },
+                          { "$gt": ["$alcohol", 0] },
+                          { "$ne": ["$literprice", None] },
+                          { "$ne": ["$alcohol", None] },
+                        ],
+                      },
+                      "then": {
+                        "$divide": ["$literprice", "$alcohol"],
+                      },
+                      "else": None,
+                    },
+                  },
+                },
+              },
             ],
             upsert=True
         )
@@ -91,16 +122,14 @@ def delete_fields(records, fields) -> BulkWriteResult:
     return _DATABASE.bulk_write(operations)
 
 
-def move(records) -> BulkWriteResult:
-    result = _DATABASE.bulk_write([
-        pymongo.DeleteOne({'index':
-                            {'$in': [record['index'] for record in records]}})
-    ])
-    print(result)
-    return _EXPIRED.bulk_write([
-        pymongo.InsertOne(record)
-        for record in records
-    ])
+def backup():
+    data = _DATABASE.find({})
+    df = pd.DataFrame(data)
+    df = df.drop(columns=["_id"])
+    df["year"] = df["year"].apply(lambda x: int(x) if x not in ("None", None) else None)
+    path = f"./backup/{pd.Timestamp.now().strftime('%d-%m-%Y')}.parquet"
+    df.to_parquet(path)
+    print("Saved backup to ", path)
 
 
 # HÅNDTER FØLGENDE PRODUKTER:
@@ -108,4 +137,4 @@ def move(records) -> BulkWriteResult:
 # "volum": {"$exists": false}
 # "bilde": {"$exists": false}
 
-# _NEW.update_many({}, { "$rename": { "indgredients": "ingredients" } })
+# _DATABASE.update_many({}, { "$rename": { "indgredients": "ingredients" } })
