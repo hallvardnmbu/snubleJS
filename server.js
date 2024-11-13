7f033e74bf02f8c299e84ebbab15e1bd09edb8e3
Hallvard Lavik <hallvard.hoyland.lavik@nmbu.no>
2024-11-13 20:06:06 +0100

    Add nynorsk.

--- server.js
+++ server.js
@@ -452,7 +452,7 @@ api.get("/id", async (req, res) => {
   }
 });
 
-// ORD APPLICATION (ord.dilettant.no)
+// ORD APPLICATION (dagsord.no)
 // ------------------------------------------------------------------------------------------------
 
 const ord = express();
@@ -477,13 +477,14 @@ try {
   process.exit(1);
 }
 const dbWord = clientWord.db("ord");
-let collectionWord = dbWord.collection("ord");
+let collectionWord = dbWord.collection("ordbok");
 
 ord.get("/random", async (req, res) => {
   try {
     const words = await collectionWord.aggregate([{ $sample: { size: 1 } }]).toArray();
     res.render("page", {
       words: words,
+      dictionary: req.query.dictionary || "bm",
       date: null,
       week: null,
       day: null,
@@ -492,6 +493,7 @@ ord.get("/random", async (req, res) => {
   } catch (error) {
     res.status(500).render("page", {
       words: [],
+      dictionary: req.query.dictionary || "bm",
       date: null,
       week: null,
       day: null,
@@ -537,6 +539,7 @@ ord.get("/search", async (req, res) => {
       .toArray();
     res.render("page", {
       words: words,
+      dictionary: req.query.dictionary || "bm",
       date: null,
       week: null,
       day: word,
@@ -545,6 +548,7 @@ ord.get("/search", async (req, res) => {
   } catch (error) {
     res.status(500).render("page", {
       words: [],
+      dictionary: req.query.dictionary || "bm",
       date: null,
       week: null,
       day: word,
@@ -554,6 +558,7 @@ ord.get("/search", async (req, res) => {
 });
 
 ord.get("/", async (req, res) => {
+  const dictionary = req.query.dictionary || "bm";
   const date = new Date();
   const week = Math.ceil(((date - new Date(date.getFullYear(), 0, 1)) / 86400000 + 1) / 7);
   const day = date.toLocaleDateString("no-NB", { weekday: "long" }).toLowerCase();
@@ -569,6 +574,7 @@ ord.get("/", async (req, res) => {
     const words = await collectionWord.find({ date: today }).toArray();
     res.render("page", {
       words: words,
+      dictionary: dictionary,
       date: today,
       week: week,
       day: day,
@@ -577,6 +583,7 @@ ord.get("/", async (req, res) => {
   } catch (error) {
     res.status(500).render("page", {
       words: [],
+      dictionary: dictionary,
       date: today,
       week: week,
       day: day,
